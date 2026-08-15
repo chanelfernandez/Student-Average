8d11f097ccde1d3aa8ac35bbc069a91c4236360a
k-david02 <kimberlybuddy2@gmail.com>
2026-08-15 02:13:31 +0000

    Grades

---
+++ main.py
@@ -0,0 +1,19 @@
+print("")
+print("Student Information")
+print("")
+name = str(input("Enter your Name: "))
+snum = int(input("Enter your Student Number: "))
+YnS = int(input("Enter your Year and Section: "))
+
+print("")
+print ('GRADE SUBJECT')
+print("")
+print ("Enter your Grades per Subject")
+
+sub1 = int(input("English : "))
+sub2 = int(input("Science : "))
+sub3 = int(input("Filipino : "))
+sub4 = int(input("Mathematics : "))
+sub5 = int(input("Social Studies : "))
+sub6 = int(input("Arts : "))
+
